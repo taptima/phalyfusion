da31015fd94400c6cbf00343d0b375d638bf9b65
Barney Laurance <barney@redmagic.org.uk>
2020-10-13 18:57:48 +0100

    Assert that all runs are sucessful.

--- tests/PhalyfusionTest.php
+++ tests/PhalyfusionTest.php
@@ -10,18 +10,20 @@ class PhalyfusionTest extends TestCase
         $runCommand = '../phalyfusion analyse -c configuration/phalyfusion_test.neon sampleCodebase/sampleNoErrors.php --no-ansi';
         $process = Process::fromShellCommandline($runCommand, __DIR__);
         $process->run();
+        $this->assertSuccessful($process);
         $this->assertStringEqualsFile(__DIR__ . '/expectedOutputs/noErrorsText.txt',$process->getOutput());
 
         $runCommand = '../phalyfusion analyse -c configuration/phalyfusion_test.neon sampleCodebase/sampleNoErrors.php -f json';
         $process = Process::fromShellCommandline($runCommand, __DIR__);
         $process->run();
+        $this->assertSuccessful($process);
         $this->assertStringEqualsFile(__DIR__ . '/expectedOutputs/noErrorsJson.txt',$process->getOutput());
 
         $runCommand = '../phalyfusion analyse -c configuration/phalyfusion_test.neon sampleCodebase/sampleNoErrors.php -f checkstyle';
         $process = Process::fromShellCommandline($runCommand, __DIR__);
         $process->run();
+        $this->assertSuccessful($process);
         $this->assertStringEqualsFile(__DIR__ . '/expectedOutputs/noErrorsCheckstyle.txt',$process->getOutput());
-
     }
 
     public function testPhalyfusion()
@@ -29,18 +31,30 @@ class PhalyfusionTest extends TestCase
         $runCommand = '../phalyfusion analyse -c configuration/phalyfusion_test.neon --no-ansi';
         $process = Process::fromShellCommandline($runCommand, __DIR__);
         $process->run();
+        $this->assertSuccessful($process);
         $this->assertStringEqualsFile(__DIR__ . '/expectedOutputs/errorsText.txt',$process->getOutput());
 
         $runCommand = '../phalyfusion analyse -c configuration/phalyfusion_test.neon -f json';
         $process = Process::fromShellCommandline($runCommand, __DIR__);
         $process->run();
+        $this->assertSuccessful($process);
         $this->assertStringEqualsFile(__DIR__ . '/expectedOutputs/errorsJson.txt',$process->getOutput());
 
         $runCommand = '../phalyfusion analyse -c configuration/phalyfusion_test.neon -f checkstyle';
         $process = Process::fromShellCommandline($runCommand, __DIR__);
         $process->run();
+        $this->assertSuccessful($process);
         $this->assertStringEqualsFile(__DIR__ . '/expectedOutputs/errorsCheckstyle.txt',$process->getOutput());
 
+    }
+
+    private function assertSuccessful(Process $process): void
+    {
+        $this->assertSame(
+            0,
+            $process->getExitCode(),
+            "\"{$process->getCommandLine()}\" returned code {$process->getExitCode()}. Error output: \n\n{$process->getErrorOutput()}"
+        );
     }
 
 }
